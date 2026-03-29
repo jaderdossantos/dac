70f9b766419443cc5685f4fcfc843262982d72cb
Jader Ribeiro <149530420+jaderdossantos@users.noreply.github.com>
2026-03-29 12:33:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const db = require('./db');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('API rodando 🚀');
+});
+
+app.get('/usuarios', (req, res) => {
+  db.query('SELECT * FROM tbUsuarios', (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.json(result);
+  });
+});
+
+app.post('/usuarios', (req, res) => {
+  const { nome, login, senha } = req.body;
+
+  db.query(
+    'INSERT INTO tbUsuarios (nome, login, senha) VALUES (?, ?, ?)',
+    [nome, login, senha],
+    (err) => {
+      if (err) return res.status(500).send(err);
+      res.send('Usuário criado');
+    }
+  );
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.query('DELETE FROM tbUsuarios WHERE usuario_id = ?', [id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.send('Usuário deletado');
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { login, senha } = req.body;
+
+  db.query(
+    'SELECT * FROM tbUsuarios WHERE login = ? AND senha = ?',
+    [login, senha],
+    (err, result) => {
+      if (err) return res.status(500).send(err);
+
+      if (result.length > 0) {
+        res.send('Login OK');
+      } else {
+        res.status(401).send('Login inválido');
+      }
+    }
+  );
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
